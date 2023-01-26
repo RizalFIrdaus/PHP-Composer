27f83a859986d380c72cc95c4eac95a354ec26d0
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-26 17:26:22 +0700

    SayHello v1.1.0

--- sayHello.php
+++ sayHello.php
@@ -5,4 +5,6 @@ require_once __DIR__ . "/vendor/autoload.php";
 use rizalfirdaus\Belajar\sayHello;
 
 $sayhello = new sayHello("Rizal", 22);
+$sayhello->setName("Fahmi Ihsan Firdaus");
+$sayhello->setAge(10);
 $sayhello->sayHello();
